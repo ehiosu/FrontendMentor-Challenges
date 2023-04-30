b292a651e2feb1df09e95eb5c5ca563c380a03eb
ehiosu <53658556+ehiosu@users.noreply.github.com>
2023-04-30 16:49:59 +0100

    Routing-Update

--- FEM/src/main.jsx
+++ FEM/src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ResultSummary } from '../Components/ResultSummary'
+import { Router,Routes,Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <ResultSummary/>
+   <Router>
+    <Routes>
+      <Route path='/' element={<ResultSummary/>}/>
+    </Routes>
+   </Router>
   </React.StrictMode>,
 )
