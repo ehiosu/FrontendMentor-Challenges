adbf882741b695988acb11443cbdb05e7e0ff2b6
ehiosu <53658556+ehiosu@users.noreply.github.com>
2023-04-30 17:00:48 +0100

    Added-react-router

--- FEM/src/main.jsx
+++ FEM/src/main.jsx
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ResultSummary } from '../Components/ResultSummary'
-import { Router,Routes,Route } from 'react-router-dom'
+import { BrowserRouter  as  Router,Routes,Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Router>
     <Routes>
-      <Route path='/' element={<ResultSummary/>}/>
+    <Route  path='/'  element={<ResultSummary/>}/>
     </Routes>
+  
    </Router>
+  
   </React.StrictMode>,
 )
